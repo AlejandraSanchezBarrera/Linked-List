4202a4b7c781dbb47c6a93d13698d8ef51902c13
AlejandraSanchezBarrera <a310401@alumnos.uaslp.mx>
2021-02-21 22:11:49 -0600

    main, clase LinkedList y Node

---
+++ src/uaslp/ingenieria/labs/list/Node.java
@@ -0,0 +1,32 @@
+package uaslp.ingenieria.labs.list;
+
+class Node {
+    private int dato;
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    private Node next;
+    private Node previous;
+}
